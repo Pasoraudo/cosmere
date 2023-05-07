6bdc6f2309a5c7e24ded67af8e12578b4a40da8e
Dinacode <dinacode@dinacode.com>
2023-05-07 15:40:09 +0200

    routing

--- src/app/ui/wiki/page/relationship/relationship.routing.ts
+++ src/app/ui/wiki/page/relationship/relationship.routing.ts
@@ -1,15 +1,9 @@
 import {Route} from '@angular/router';
-import {HomePage} from 'app/ui/wiki/page/home/home.page';
 import {RelationshipPage} from './relationship.page';
 
 export const relationshipRouting: Route[] = [
   {
     path: '',
-    redirectTo: 'relationship',
-    pathMatch: 'full',
-  },
-  {
-    path: 'relationship',
     component: RelationshipPage,
   },
 ];
