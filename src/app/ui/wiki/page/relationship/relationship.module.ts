193546cd6897a3bce1b0eef0d788a436eb74fe1e
Dinacode <dinacode@dinacode.com>
2023-06-06 19:49:56 +0200

    analysis clean

--- src/app/ui/wiki/page/relationship/relationship.module.ts
+++ src/app/ui/wiki/page/relationship/relationship.module.ts
@@ -5,17 +5,19 @@ import {relationshipRouting} from './relationship.routing';
 import {CharactersRelationshipsGraphModule} from './component/characters-relationships-graph/characters-relationships-graph.module';
 import {CdkScrollableModule} from '@angular/cdk/scrolling';
 import {FuseCardModule} from '../../../../../@fuse/components/card';
+import {SharedModule} from '../../../shared/components/shared.module';
 
 @NgModule({
   declarations: [
     RelationshipPage
   ],
-  imports: [
-    RouterModule.forChild(relationshipRouting),
-    CharactersRelationshipsGraphModule,
-    CdkScrollableModule,
-    FuseCardModule
-  ]
+    imports: [
+        RouterModule.forChild(relationshipRouting),
+        CharactersRelationshipsGraphModule,
+        CdkScrollableModule,
+        FuseCardModule,
+        SharedModule
+    ]
 })
 export class RelationshipModule {
 }
