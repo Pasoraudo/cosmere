06eea3e4fbf1da4a167e92a9f28264b10638a01d
Dinacode <dinacode@dinacode.com>
2023-06-08 20:04:16 +0200

    home done

--- src/domain/model/book.ts
+++ src/domain/model/book.ts
@@ -1,6 +1,7 @@
 import {Entity} from './shared.model';
 
-export const cosmereBookIds = () => ['warbreaker', 'mistborn1', 'mistborn2', 'mistborn3', 'stormlight1'];
+export const cosmereBookIds = () => ['warbreaker', 'mistborn1', 'mistborn2', 'mistborn3', 'mistborn4', 'mistborn5', 'mistborn6'
+  , 'stormlight1', 'stormlight2', 'stormlight3', 'stormlight4'];
 export interface Book extends Entity {
   title: string;
 
