3c2295ba11cabf2b5fd8523642f4c7ee6589ba85
Dinacode <dinacode@dinacode.com>
2023-06-12 21:58:18 +0200

    build

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+import 'zone.js/testing';
+import 'zone.js';
+import 'zone.js/dist/async-test.js';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
