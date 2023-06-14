b2b0677c168424978d664aacc4f610e481f2b303
Dinacode <dinacode@dinacode.com>
2023-06-14 21:05:51 +0200

    deploy

--- src/app/ui/infrastructure/d3/component/network.component.ts
+++ src/app/ui/infrastructure/d3/component/network.component.ts
@@ -168,7 +168,7 @@ export class D3NetworkComponent extends BaseComponent implements AfterViewInit, 
   }
 
   private linkArc(d) {
-    const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
+    const r = 0;
     return `
     M${d.source.x},${d.source.y}
     A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
