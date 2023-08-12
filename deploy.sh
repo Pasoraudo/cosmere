948bae4aa3782d889bce7d94f562cf2bdc561d67
Dinacode Developer <developer@dinacode.com>
2023-08-12 16:26:28 +0200

    deplot archive

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Ejecutar ng deploy con base-href
-ng deploy --base-href=/cosmere/
+# Ejecutar ng deploy.sh con base-href
+ng deploy.sh --base-href=/cosmere/
 
 # Agregar todos los cambios al área de preparación de Git
 git add -A
 
-# Hacer un commit con el mensaje "deploy"
+# Hacer un commit con el mensaje "deploy.sh"
 git commit -m "deploy"
 
 # Empujar los cambios al repositorio remoto (origin) en la rama master
